082d3602d75d3b7e11360dc9a8fc38f833c4e6cd
sunitha22007 <sunitha22007@gmail.com>
2026-06-11 15:58:30 +0530

    Added Hangman Game

--- Hangman game.py
+++ Hangman game.py
@@ -1,1 +1,25 @@
-create Hangman game.py
+import random
+words = ["python", "apple", "computer", "program", "school"]
+word = random.choice(words)
+guessed = []
+attempts = 6
+while attempts > 0:
+    display = ""
+    for letter in word:
+        if letter in guessed:
+            display += letter + " "
+        else:
+            display += "_ "
+    print(display)
+    if "_" not in display:
+        print("You Win!")
+        break
+    guess = input("Enter a letter: ")
+    if guess in word:
+        guessed.append(guess)
+    else:
+        attempts -= 1
+        print("Wrong! Attempts left:", attempts)
+if attempts == 0:
+    print("You Lost! Word was:", word)
+    
